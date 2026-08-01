3663494ff76e34cf70f21fe57c665d480331f853
moamenelkassas43-sudo <moamenelkassas43@gmail.com>
2026-08-01 23:35:45 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', (e) => {
+    // يمنع إعادة تحميل الصفحة ويقوم بحفظ الاسم والبريد والرسالة داخل localStorage
+});
+const themeToggleBtn = document.getElementById('themeToggleBtn');
+// يقوم بقراءة التفضيل المحفوظ في المتصفح وتغيير قيمة data-theme في عنصر html
